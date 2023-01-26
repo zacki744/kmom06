afc4ce007a4e9073fc3dfff33978d7c5a114f5e1
zacki744 <78082188+zacki744@users.noreply.github.com>
2023-01-26 15:01:40 +0100

    fix pasword for this iteration

--- eshop2/sql/eshop/insert.sql
+++ eshop2/sql/eshop/insert.sql
@@ -3,6 +3,8 @@ use `eshop`;
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET SQL_SAFE_UPDATES = 0;
+
 --
 -- Enable LOAD DATA LOCAL INFILE on the server.
 --
@@ -32,7 +34,7 @@ LINES
 --
 -- loade objekts from csv into Kund_Register
 --
-LOAD DATA INFILE 'C:/cygwin64/home/zacki/databas/me/kmom06/eshop2/sql/eshop/Kund_register.csv'
+LOAD DATA INFILE 'C:/Users/zacki/OneDrive/Desktop/programering/datorteknik/kmom06/eshop2/sql/eshop/Kund_register.csv'
 INTO TABLE `eshop`.`Kund_Register`
 CHARSET utf8
 FIELDS
@@ -46,7 +48,7 @@ LINES
 --
 -- loade objekts from csv into produktregister
 --
-LOAD DATA INFILE 'C:/cygwin64/home/zacki/databas/me/kmom06/eshop2/sql/eshop/produktregister.csv'
+LOAD DATA INFILE 'C:/Users/zacki/OneDrive/Desktop/programering/datorteknik/kmom06/eshop2/sql/eshop/produktregister.csv'
 INTO TABLE `eshop`.`produktregister`
 CHARSET utf8
 FIELDS
@@ -60,7 +62,7 @@ LINES
 --
 -- loade objekts from csv into coffe
 --
-LOAD DATA INFILE 'C:/cygwin64/home/zacki/databas/me/kmom06/eshop2/sql/eshop/coffie.csv'
+LOAD DATA INFILE 'C:/Users/zacki/OneDrive/Desktop/programering/datorteknik/kmom06/eshop2/sql/eshop/coffie.csv'
 INTO TABLE `eshop`.`kaffe`
 CHARSET utf8
 FIELDS
